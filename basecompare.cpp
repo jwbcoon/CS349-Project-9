628e60b10d546cb6587ee51b976f7db7404a0612
jwbcoon <89538296+jwbcoon@users.noreply.github.com>
2023-04-14 11:31:12 -0700

    Update basecompare.cpp

--- basecompare.cpp
+++ basecompare.cpp
@@ -2,6 +2,13 @@
 // Created by student on 4/14/23.
 //
 
+#include "commonLib.h"
+
+using namespace std;
+
+enum { ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
+        A, B, C, D, E, F, G, H, I, J, K, L, M,
+        N, O, P, Q, R, S, T, U, V, W, X, Y, Z } bases;
 
 int main(int argc, char** argv) {
     return 0;
